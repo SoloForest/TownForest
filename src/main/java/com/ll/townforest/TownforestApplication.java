0af8d6a4f3099a7de0353a1491140a6fb8d20607
깃ChertHyeon <r4560798@naver.com>
2023-06-22 14:18:04 +0900

    init

---
+++ src/main/java/com/ll/townforest/TownforestApplication.java
@@ -0,0 +1,13 @@
+package com.ll.townforest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TownforestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TownforestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/townforest/TownforestApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ll.townforest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class TownforestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
