5173ab039111ff5e2bf87160e5fc63f38636b73b
elephantLikesApple <elephant890@naver.com>
2023-06-26 16:19:29 +0900

    fix: Library에 빠진 Apt 변수 생성 및 컨벤션에 맞지 않는 변수명 변경

--- src/main/java/com/ll/townforest/boundedContext/library/entity/Library.java
+++ src/main/java/com/ll/townforest/boundedContext/library/entity/Library.java
@@ -2,12 +2,15 @@ package com.ll.townforest.boundedContext.library.entity;
 
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import com.ll.townforest.boundedContext.apt.entity.Apt;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -28,12 +31,15 @@ public class Library {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@ManyToOne
+	private Apt apart;
+
 	@Column(nullable = false)
 	private String name;
 
 	@Column(nullable = false)
-	private Integer max_people;
+	private Integer maxPeople;
 
 	// TODO: Picture 생성해서 이미지 받아올 수 있도록 만들기
-	// private MultipartFile seating_chart;
+	// private MultipartFile seatingChart;
 }
