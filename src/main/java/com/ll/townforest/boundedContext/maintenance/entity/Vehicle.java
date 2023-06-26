0521e7edccd3d09a69eee59a50ec00933102bad3
깃chan0521 <aflpq369@gmail.com>
2023-06-26 16:16:27 +0900

    feat : push문제 해결

--- src/main/java/com/ll/townforest/boundedContext/maintenance/entity/Vehicle.java
+++ src/main/java/com/ll/townforest/boundedContext/maintenance/entity/Vehicle.java
@@ -2,6 +2,8 @@ package com.ll.townforest.boundedContext.maintenance.entity;
 
 import java.time.LocalDateTime;
 
+import com.ll.townforest.boundedContext.apt.entity.AptAccount;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,6 +11,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -18,16 +21,19 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Builder
 public class Vehicle {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	@ManyToOne
 	private AptAccount user;
-	@Column(length = 8)
+	@Column(length = 10)
 	private String vehicleNumber;
-	@Column(columnDefinition = "TEXT")
+	@Column(nullable = false)
 	private String name;
-	private int type; // 방문차량(1), 세대주 차량(0) 구분
+	@Column(nullable = false)
+	private Integer type; // 방문차량(1), 세대주 차량(0) 구분
 	private LocalDateTime date; // 방문일
+
 }
