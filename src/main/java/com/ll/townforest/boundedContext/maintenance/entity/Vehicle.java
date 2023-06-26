3445e626286a3daab5b6c341131c25b7e5245273
깃chan0521 <aflpq369@gmail.com>
2023-06-26 15:03:12 +0900

    feat : 차량등록 엔티티작성

---
+++ src/main/java/com/ll/townforest/boundedContext/maintenance/entity/Vehicle.java
@@ -0,0 +1,33 @@
+package com.ll.townforest.boundedContext.maintenance.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Vehicle {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@ManyToOne
+	AptAccount user;
+	@Column(length = 8)
+	private String vehicleNumber;
+	@Column(columnDefinition = "TEXT")
+	private String name;
+	private int type; // 방문차량(1), 세대주 차량(0) 구분
+	private LocalDateTime date; // 방문일
+}
