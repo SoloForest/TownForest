7367799cba7fee600f53b8e08ba14c753bb74fe8
ujineeee <yujin11006@naver.com>
2023-06-26 14:37:57 +0900

    feat : 회원 엔티티 작성

---
+++ src/main/java/com/ll/townforest/boundedContext/account/entity/Account.java
@@ -0,0 +1,38 @@
+package com.ll.townforest.boundedContext.account.entity;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+public class Account {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(unique = true)
+	private String userId;
+	@Column(nullable = false)
+	private String password;
+	@Column(nullable = false)
+	private String username;
+	@Column(unique = true)
+	private String email;
+	@Column(unique = true)
+	private String phoneNumber;
+}
