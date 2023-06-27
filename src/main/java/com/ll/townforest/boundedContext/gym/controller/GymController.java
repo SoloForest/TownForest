09a0aed2fca51bb9feaba856b69af95cc99b5d38
깃ChertHyeon <r4560798@naver.com>
2023-06-27 20:05:19 +0900

    feat : locker & refund html 작성

--- src/main/java/com/ll/townforest/boundedContext/gym/controller/GymController.java
+++ src/main/java/com/ll/townforest/boundedContext/gym/controller/GymController.java
@@ -2,16 +2,28 @@ package com.ll.townforest.boundedContext.gym.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/gym")
 public class GymController {
 
-	@GetMapping("/gym")
-	public String root() {
+	@GetMapping("")
+	public String gymMain() {
 		return "gym/gym";
+	}
+
+	@GetMapping("/locker")
+	public String gymLocker() {
+		return "gym/locker";
+	}
+
+	@GetMapping("/refund")
+	public String gymRefund() {
+		return "gym/refund";
 	}
 
 }
