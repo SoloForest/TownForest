50146fac01a24c13cb7ebdacfa9ce5cc9f4df0ea
ujineeee <yujin11006@naver.com>
2023-07-03 14:13:13 +0900

    refactor : email에 @Pattern 추가 및 안내 문구 변경

--- src/main/java/com/ll/townforest/boundedContext/account/dto/AccountDTO.java
+++ src/main/java/com/ll/townforest/boundedContext/account/dto/AccountDTO.java
@@ -1,6 +1,7 @@
 package com.ll.townforest.boundedContext.account.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,6 +19,7 @@ public class AccountDTO {
 	@Size(min = 2, max = 10)
 	String fullName;
 	@NotBlank
+	@Pattern(regexp = "^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$", message = "이메일 형식이 아닙니다.")
 	String email;
 	@NotBlank
 	@Size(min = 10, max = 11)
--- src/main/java/com/ll/townforest/boundedContext/account/service/AccountService.java
+++ src/main/java/com/ll/townforest/boundedContext/account/service/AccountService.java
@@ -34,15 +34,15 @@ public class AccountService {
 
 	public RsData<Account> join(AccountDTO dto) {
 		if (findByUsername(dto.getUsername()).isPresent()) {
-			return RsData.of("F-1", "사용할 수 없는 아이디입니다.");
+			return RsData.of("F-1", "사용할 수 없는 아이디입니다.<br>다른 아이디를 입력해 주세요.");
 		}
 
 		if (findByEmail(dto.getEmail()).isPresent()) {
-			return RsData.of("F-2", "사용할 수 없는 이메일입니다.");
+			return RsData.of("F-2", "사용할 수 없는 이메일입니다.<br>다른 이메일을 입력해 주세요.");
 		}
 
 		if (findByPhoneNumber(dto.getPhoneNumber()).isPresent()) {
-			return RsData.of("F-3", "사용할 수 없는 휴대전화번호입니다.");
+			return RsData.of("F-3", "사용할 수 없는 휴대전화번호입니다.<br>다른 휴대전화번호를 입력해 주세요.");
 		}
 
 		String password = passwordEncoder.encode(dto.getPassword());
