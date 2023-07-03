319e9c3e78100e084357b7e2fb08a3b9c7fb3fe6
elephantLikesApple <elephant890@naver.com>
2023-07-04 02:05:18 +0900

    feat: 관리자 main 페이지 작성

--- src/main/java/com/ll/townforest/boundedContext/home/controller/AdminController.java
+++ src/main/java/com/ll/townforest/boundedContext/home/controller/AdminController.java
@@ -1,8 +1,11 @@
 package com.ll.townforest.boundedContext.home.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ll.townforest.base.rq.Rq;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,13 +13,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminController {
-	@GetMapping("")
-	public String showAdmin() {
-		return "admin/main";
-	}
+	private final Rq rq;
 
-	@GetMapping("/library/histories")
-	public String showLibraryHistories() {
-		return "admin/library/histories";
+	@GetMapping("")
+	@PreAuthorize("isAuthenticated()")
+	public String showAdmin() {
+		if (rq.getAptAccount().getAuthority() == 0) {
+			return "redirect:/";
+		}
+		return "admin/main";
 	}
 }
