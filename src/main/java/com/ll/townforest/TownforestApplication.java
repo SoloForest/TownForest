f7e205b66d7838b0fd3fc6129296ab5b9d5b7d6d
elephantLikesApple <elephant890@naver.com>
2023-07-04 13:10:17 +0900

    feat: 자정에 모든 이용중인 독서실 자리를 취소처리

--- src/main/java/com/ll/townforest/TownforestApplication.java
+++ src/main/java/com/ll/townforest/TownforestApplication.java
@@ -3,9 +3,11 @@ package com.ll.townforest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing // @EntityListeners(AuditingEntityListener.class) 가 작동하도록 허용
+@EnableScheduling // @Scheduled 가 작동하도록 허용
 public class TownforestApplication {
 
 	public static void main(String[] args) {
