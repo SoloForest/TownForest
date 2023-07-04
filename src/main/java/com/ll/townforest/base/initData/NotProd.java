388ee32547fda3281f0f8c83e0b3e25f39df9195
ujineeee <yujin11006@naver.com>
2023-07-04 12:55:26 +0900

    feat : 내 아파트 정보 등록 기능 구현

--- src/main/java/com/ll/townforest/base/initData/NotProd.java
+++ src/main/java/com/ll/townforest/base/initData/NotProd.java
@@ -134,6 +134,7 @@ public class NotProd {
 							House houseTemp = House.builder()
 								.dong(100 + i)
 								.ho((j * 100) + k)
+								.apt(apt1)
 								.build();
 							houseList1.add(houseTemp);
 						}
@@ -149,6 +150,7 @@ public class NotProd {
 							House houseTemp = House.builder()
 								.dong(100 + i)
 								.ho((j * 100) + k)
+								.apt(apt1)
 								.build();
 							houseList2.add(houseTemp);
 						}
@@ -164,6 +166,7 @@ public class NotProd {
 							House houseTemp = House.builder()
 								.dong(100 + i)
 								.ho((j * 100) + k)
+								.apt(apt1)
 								.build();
 							houseList3.add(houseTemp);
 						}
@@ -231,7 +234,7 @@ public class NotProd {
 				AptAccountHouse aptAccountHouse1 = AptAccountHouse.builder()
 					.relationship("세대주")
 					.user(aptAccount4)
-					.house(houseRepository.findByDongAndHo(103, 2101))
+					.house(houseRepository.findByAptAndDongAndHo(apt1, 103, 2101).get())
 					.build();
 				aptAccountHouseRepository.save(aptAccountHouse1);
 
--- src/main/java/com/ll/townforest/boundedContext/apt/service/AptAccountService.java
+++ src/main/java/com/ll/townforest/boundedContext/apt/service/AptAccountService.java
@@ -5,9 +5,18 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ll.townforest.base.rsData.RsData;
 import com.ll.townforest.boundedContext.account.entity.Account;
+import com.ll.townforest.boundedContext.account.service.AccountService;
+import com.ll.townforest.boundedContext.apt.DTO.AptAccountDTO;
+import com.ll.townforest.boundedContext.apt.entity.Apt;
 import com.ll.townforest.boundedContext.apt.entity.AptAccount;
+import com.ll.townforest.boundedContext.apt.entity.AptAccountHouse;
+import com.ll.townforest.boundedContext.apt.entity.House;
+import com.ll.townforest.boundedContext.apt.repository.AptAccountHouseRepository;
 import com.ll.townforest.boundedContext.apt.repository.AptAccountRepository;
+import com.ll.townforest.boundedContext.apt.repository.AptRepository;
+import com.ll.townforest.boundedContext.apt.repository.HouseRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,9 +24,50 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class AptAccountService {
+	private final AccountService accountService;
+	private final AptRepository aptRepository;
+	private final HouseRepository houseRepository;
 	private final AptAccountRepository aptAccountRepository;
+	private final AptAccountHouseRepository aptAccountHouseRepository;
 
 	public Optional<AptAccount> findByAccount(Account account) {
 		return aptAccountRepository.findByAccount(account);
+	}
+
+	public RsData<AptAccount> canRegister(Account account, AptAccountDTO aptAccountDTO) {
+		Apt apt = aptRepository.findByName(aptAccountDTO.getAptName()).orElse(null);
+
+		if (apt == null) {
+			return RsData.of("F-1", "등록되지 않은 아파트입니다.");
+		}
+
+		House house = houseRepository.findByAptAndDongAndHo(apt, aptAccountDTO.getDong(), aptAccountDTO.getHo())
+			.orElse(null);
+
+		if (house == null) {
+			return RsData.of("F-2", "거주하시는 동과 호수를 바르게 입력해 주세요.");
+		}
+
+		return register(account, apt, house, aptAccountDTO.getRelationship());
+	}
+
+	@Transactional
+	public RsData<AptAccount> register(Account account, Apt apt, House house, String relationship) {
+		AptAccount aptAccount = AptAccount.builder()
+			.account(account)
+			.apt(apt)
+			.build();
+
+		aptAccountRepository.save(aptAccount);
+
+		AptAccountHouse aptAccountHouse = AptAccountHouse.builder()
+			.relationship(relationship)
+			.user(aptAccount)
+			.house(house)
+			.build();
+
+		aptAccountHouseRepository.save(aptAccountHouse);
+
+		return RsData.of("S-1", "내 아파트 등록 요청을 완료했습니다.<br>관리자의 승인을 기다려주세요.");
 	}
 }
