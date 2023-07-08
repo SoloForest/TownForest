574b4078e974e0fee98d30a514cc9b6725a72117
ujineeee <yujin11006@naver.com>
2023-07-09 01:34:35 +0900

    feat : 관리자의 회원 승인 기능 구현

--- src/main/java/com/ll/townforest/boundedContext/apt/service/AptAccountHouseService.java
+++ src/main/java/com/ll/townforest/boundedContext/apt/service/AptAccountHouseService.java
@@ -1,12 +1,16 @@
 package com.ll.townforest.boundedContext.apt.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ll.townforest.base.rsData.RsData;
+import com.ll.townforest.boundedContext.apt.entity.AptAccount;
 import com.ll.townforest.boundedContext.apt.entity.AptAccountHouse;
 import com.ll.townforest.boundedContext.apt.repository.AptAccountHouseRepository;
+import com.ll.townforest.boundedContext.apt.repository.AptAccountRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,7 +18,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class AptAccountHouseService {
+	private final AptAccountRepository aptAccountRepository;
 	private final AptAccountHouseRepository aptAccountHouseRepository;
+
+	public Optional<AptAccountHouse> findById(Long id) {
+		return aptAccountHouseRepository.findById(id);
+	}
 
 	public List<AptAccountHouse> findAptAccountHouse(int sortCode) {
 
@@ -23,5 +32,29 @@ public class AptAccountHouseService {
 			case 3 -> aptAccountHouseRepository.findByUser_StatusFalse();
 			default -> aptAccountHouseRepository.findAllDesc();
 		};
+	}
+
+	@Transactional
+	public RsData<AptAccountHouse> canApprove(AptAccount aptAccount, AptAccountHouse aptAccountHouse) {
+		if (aptAccount.getAuthority() != 1) {
+			return RsData.of("F-1", "승인할 권한이 없습니다.");
+		}
+
+		if (aptAccountHouse == null) {
+			return RsData.of("F-2", "존재하지 않는 회원 입니다.");
+		}
+
+		return approve(aptAccountHouse);
+	}
+
+	@Transactional
+	public RsData<AptAccountHouse> approve(AptAccountHouse aptAccountHouse) {
+		AptAccount aptAccount = aptAccountHouse.getUser();
+		AptAccount modifyAptAccount = aptAccount.toBuilder()
+			.status(true)
+			.build();
+
+		aptAccountRepository.save(modifyAptAccount);
+		return RsData.of("S-1", "승인되었습니다.");
 	}
 }
--- src/main/java/com/ll/townforest/boundedContext/home/controller/AdminController.java
+++ src/main/java/com/ll/townforest/boundedContext/home/controller/AdminController.java
@@ -8,6 +8,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -82,11 +83,36 @@ public class AdminController {
 	}
 
 	@GetMapping("/management")
+	@PreAuthorize("isAuthenticated()")
 	public String showAptAccountManagement(Model model, @RequestParam(defaultValue = "1") int sortCode) {
+		int authority = rq.getAptAccount().getAuthority();
+
+		if (authority == 0) {
+			return "redirect:/";
+		}
+
+		if (authority == 2 || authority == 3) {
+			return "redirect:/admin";
+		}
+
 		List<AptAccountHouse> aptAccountHouseList = aptAccountHouseService.findAptAccountHouse(sortCode);
-		
+
 		model.addAttribute("aptAccountHouseList", aptAccountHouseList);
 		model.addAttribute("sortCode", sortCode);
 		return "admin/aptAccount/management";
+	}
+
+	@PostMapping("/approve/{id}")
+	public String approve(@PathVariable Long id, @RequestParam int sortCode) {
+		AptAccountHouse aptAccountHouse = aptAccountHouseService.findById(id).orElse(null);
+
+		RsData<AptAccountHouse> aptAccountHouseRsData = aptAccountHouseService.canApprove(rq.getAptAccount(),
+			aptAccountHouse);
+
+		if (aptAccountHouseRsData.isFail()) {
+			return rq.historyBack(aptAccountHouseRsData);
+		}
+
+		return rq.redirectWithMsg("/admin/management?sortCode=%d".formatted(sortCode), aptAccountHouseRsData);
 	}
 }
