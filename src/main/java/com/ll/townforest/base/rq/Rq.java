83778cc50f604523b0a92b97bc060adf2e794fe5
ujineeee <yujin11006@naver.com>
2023-07-10 01:52:25 +0900

    feat : 관리자 로그인 시 관리자 페이지 연결

--- src/main/java/com/ll/townforest/base/rq/Rq.java
+++ src/main/java/com/ll/townforest/base/rq/Rq.java
@@ -59,6 +59,12 @@ public class Rq {
 		return !isLogin();
 	}
 
+	//관리자인지 체크
+	public boolean isAdmin() {
+		getAptAccount();
+		return aptAccount.getAuthority() != 0;
+	}
+
 	// 로그인 된 회원의 객체
 	public Account getAccount() {
 		if (isLogout())
