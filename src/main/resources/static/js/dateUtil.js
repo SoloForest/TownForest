9b331be4ac503357c8d25192924db3a3244f8322
elephantLikesApple <elephant890@naver.com>
2023-07-11 00:11:32 +0900

    feat: 날짜 포멧 변경하는 js로직 추가. 시간 없이 날짜만 표시

--- src/main/resources/static/js/dateUtil.js
+++ src/main/resources/static/js/dateUtil.js
@@ -1,9 +1,16 @@
 function dateFormat(dateString) {
     const date = new Date(dateString);
     return date.getFullYear() +
-        '.' + ((date.getMonth() + 1) < 9 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1)) +
+        '.' + ((date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1)) +
         '.' + (date.getDate() < 10 ? "0" + date.getDate() : date.getDate()) +
         ' ' + (date.getHours() < 10 ? "0" + date.getHours() : date.getHours()) +
         ':' + (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()) +
         ':' + (date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds());
+}
+
+function dateFormatWithoutTime(dateString) {
+    const date = new Date(dateString);
+    return date.getFullYear() +
+        '.' + ((date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1)) +
+        '.' + (date.getDate() < 10 ? "0" + date.getDate() : date.getDate());
 }
